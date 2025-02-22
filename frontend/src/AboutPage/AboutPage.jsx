8a24a82e03ef2f989f17fd8078758879d9b141c2
Mufaddal Kapadia <161473458+forcedflame@users.noreply.github.com>
2025-02-21 23:10:28 -0500

    finished about us page

--- frontend/src/AboutPage/AboutPage.jsx
+++ frontend/src/AboutPage/AboutPage.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './AboutPage.css';
 import Pfp1 from '../Assets/omR.jpeg';
 import Pfp2 from '../Assets/RonithN.jpeg';
+import Pfp4 from '../Assets/MufK.jpg';
+import Pfp5 from '../Assets/NikilP.jpeg';
+import defMpfp from '../Assets/defaultMpfp.png';
 
 function AboutUsPage() {
     return (
@@ -12,7 +15,7 @@ function AboutUsPage() {
                 <div className="hero-content">
                     <h1 className="hero-title">Who are we?</h1>
                     <p className="hero-subtitle">
-                        An ambitious organization comprised of hardworking students that have a passion to make learning accessible and to cultivate academic growth.
+                        An ambitious organization comprised of hardworking students that have a passion to make learning straightforward and to maximize academic growth efficiency.
                     </p>
                 </div>
             </section>
@@ -20,10 +23,10 @@ function AboutUsPage() {
             {/* Mission Section */}
             <section className="about-section mission-section">
                 <div className="section-content">
-                    <h1 className="section-title">Our Mission Statement</h1>
+                    <h1 className="statement-title">Our Mission Statement</h1>
                     <div className="mission-content">
                         <p>
-                            "Our mission is to provide free accessible materials and guidance for students from all backgrounds to utilize."
+                            "Our mission is to provide free accessible materials and straightforward guidance for students from all backgrounds to utilize to prevent future regrets."
                         </p>
                     </div>
                 </div>
@@ -32,7 +35,7 @@ function AboutUsPage() {
             {/* Values Section - DONE*/}
             <section className="about-section values-section">
                 <div className="section-content">
-                    <h2 className="section-title">Our Company's Values to SUCCEED</h2>
+                    <h2 className="values-title">Our Company's Values to SUCCEED</h2>
                     <div className="values-grid">
                         <div className="value-card">
 
@@ -49,7 +52,7 @@ function AboutUsPage() {
                         </div>
                         <div className="value-card">
                             <h2 className="value-name">Creativity</h2>
-                            <p className="value-content">We come up with ways to perfect our site along with actually implementing them</p>
+                            <p className="value-content">We come up with unique ways to perfect our site along with actually implementing them.</p>
                         </div>
                         <div className="value-card">
                             <h2 className="value-name">Enthusiasm</h2>
@@ -69,20 +72,70 @@ function AboutUsPage() {
 
             {/* Team Section */}
             <section className="about-section team-section">
-                <div className="section-content">
-                    <h2 className="section-title">Meet our team!</h2>
+                <div className="team-content">
+                    <h2 className="team-title">Meet our team!</h2>
                     <div className="team-grid">
                         {/* Team member cards will go here */}
                         <div className="team-card" id="OmR">
-                            <img className="card-image" src={Pfp1}></img>
-                            <h2>Om Raheja - Chief Executive Officer (CEO)</h2>
-                            <p></p>
+                            <div className="card-image">
+                                <img src={Pfp1} alt="Om Raheja"></img>
+                            </div>
+                            <div className="team-card-content">
+                                <h2>Om Raheja</h2>
+                                <a className="exclusive-emails" href="mailto:om@aquarc.org">om@aquarc.org</a>
+                                <p>Founder</p>
+                            </div>
                         </div>
-                        <div className="team-card" id="RonithN"></div>
-                            <img className="card-image" src={Pfp2}></img>
-                            <h2>Ronith Neelam - Chief Technology Officer (CTO)</h2>
-                            <p></p>
-                        <div className="team-card"></div>
+                        <div className="team-card" id="RonithN">
+                            <div className="card-image">
+                                <img src={Pfp2} alt="Ronith Neelam"></img>
+                            </div>
+                            <div>
+                                <h2>Ronith Neelam</h2>
+                                <a className="exclusive-emails" href="mailto:ronith@aquarc.org">ronith@aquarc.org</a>
+                                <p>Tech Lead</p>
+                            </div>
+                        </div>
+                        <div className="team-card" id="SidA">
+                            <div className="card-image">
+                                <img src={defMpfp} alt="Sid Alapati"></img>
+                            </div>
+                            <div>
+                                <h2>Sid Alapati</h2>
+                                <a className="exclusive-emails" href="mailto:sid@aquarc.org">sid@aquarc.org</a>
+                                <p>Full-Stack Dev</p>
+                            </div>
+                        </div>
+                        <div className="team-card" id="MufaddalK">
+                            <div className="card-image">
+                                <img src={Pfp4} alt="Mufaddal Kapadia"></img>
+                            </div>
+                            <div>
+                                <h2>Mufaddal Kapadia</h2>
+                                <a className="exclusive-emails" href="mailto:mufaddal@aquarc.org">mufaddal@aquarc.org</a>
+                                <p>Frontend Dev</p>
+                            </div>
+                        </div>
+                        <div className="team-card" id="NikilP">
+                            <div className="card-image">
+                                <img src={Pfp5} alt="Nikil Pakianathan"></img>
+                            </div>
+                            <div>
+                                <h2>Nikil Pakianathan</h2>
+                                <a className="exclusive-emails" href="mailto:nikil@aquarc.org">nikil@aquarc.org</a>
+                                <p>Full-Stack Dev</p>
+                            </div>
+                        </div>
+                        <div className="team-card" id="Riley">
+                            <div className="card-image">
+                                <img src={defMpfp} alt="Riley"></img>
+                            </div>
+                            <div>
+                                <h2>Riley</h2>
+                                <a className="exclusive-emails" href="mailto:riley@aquarc.org">riley@aquarc.org</a>
+                                <p>Dev</p>
+                            </div>
+                        </div>
 
                     </div>
                 </div>
@@ -92,8 +145,11 @@ function AboutUsPage() {
             {/* Contact Section */}
             <section className="about-section contact-section">
                 <div className="section-content">
-                    <h2 className="section-title"></h2>
-                    <div className="contact-content"></div>
+                    <h2 className="contact-title">Contact Us!</h2>
+                    <div className="contact-content">
+                        <h2>Email us at </h2>
+                        <a href="mailto:contact@aquarc.org">contact@aquarc.org</a>
+                    </div>
                 </div>
             </section>
         </div>
