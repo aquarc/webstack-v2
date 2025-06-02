b94734ab58918afa6badcbbf043dc20b6c34dd1f
Om Raheja <om@omraheja.me>
2025-06-01 22:48:14 -0400

    feat(filtesr): e

--- frontend/src/SatPage/SatPage.jsx
+++ frontend/src/SatPage/SatPage.jsx
@@ -1551,7 +1551,7 @@ const handleSimilarQuestions = async () => {
 
           {!userEmail && (
             <>
-              <p><i style={{ color: 'red' }}>Please sign in for practice test mode.</i></p>
+              <p><i style={{ color: 'red' }}>Please sign in for 15-question practice sets.</i></p>
               {renderSignUpForm()}
             </>
           )}
